aba321fde7290268fa7790487a2191de676b78ec
webgori <webgori@gmail.com>
2020-03-30 09:42:38 +0900

    프로젝트 생성

---
+++ src/main/java/kr/webgori/demo/website/api/DemoWebsiteApiApplication.java
@@ -0,0 +1,13 @@
+package kr.webgori.demo.website.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebsiteApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebsiteApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/webgori/demo/website/api/DemoWebsiteApiApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.webgori.demo.website.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebsiteApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
